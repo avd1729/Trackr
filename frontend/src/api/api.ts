698e3407017f548c2b535c009d884a0d03719364
Aravind <aravind19112003@gmail.com>
2025-08-19 19:47:55 +0530

    add api methods for get all tasks and creating a task

---
+++ frontend/src/api/api.ts
@@ -0,0 +1,58 @@
+import type { Task } from "@/model/Task";
+import type { TaskDTO } from "@/dto/TaskDTO";
+
+async function getAllTasks(): Promise<Array<Task>> {
+  const response = await fetch("http://localhost:8080/api/task");
+  if (!response.ok) {
+    throw new Error("Failed to fetch tasks");
+  }
+  return await response.json();
+}
+
+async function createTask(taskDTO: TaskDTO): Promise<Task> {
+
+    const formattedDueDate = new Date(taskDTO.dueDate).toISOString().slice(0, 19);
+
+    const task: Task = {
+        title: taskDTO.title,
+        description: taskDTO.description,
+        category: taskDTO.category,
+        dueDate: formattedDueDate,
+        completed: taskDTO.completed,
+    };
+
+    const response = await fetch("http://localhost:8080/api/task", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(task),
+    });
+
+    if (!response.ok) {
+        throw new Error("Failed to create task");
+    }
+
+    return await response.json();
+}
+
+
+(async () => {
+  const newTask: TaskDTO = {
+    title: "Learn TanStack",
+    description: "Explore query + router",
+    dueDate: "2025-08-20",
+    category: "Learning",
+    completed: false,
+  };
+
+  try {
+    const createdTask = await createTask(newTask);
+    console.log("Task created:", createdTask);
+
+    const tasks = await getAllTasks();
+    console.log("All tasks:", tasks);
+  } catch (err) {
+    console.error("Error:", err);
+  }
+})();
